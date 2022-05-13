ff0164acf89a01c1aad54c3d8142d1ca93baeedb
Larissa Araujo <larissa.jvsa@gmail.com>
2022-05-13 17:46:44 -0300

    feat: :ambulance: add error middleware

---
+++ middlewares/handleErrorMiddleware.js
@@ -0,0 +1,8 @@
+const throwMiddleware = (err, _req, res, _next) => {
+  if (err.status) return res.status(err.status).json({ message: err.message });
+  return res.status(500).json({ message: 'Internal Server Error' });
+};
+
+module.exports = {
+  throwMiddleware,
+};
