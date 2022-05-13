2d7d7bd97c1b463ebd6da7d59f7c5b2a625efc39
Larissa Araujo <larissa.jvsa@gmail.com>
2022-05-13 17:39:52 -0300

    feat: :ambulance: add /products /products:id and /sales /sales:id

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const { throwMiddleware } = require('./middlewares/handleErrorMiddleware');
+const controller = require('./controllers');
 
 const app = express();
+app.use(bodyParser.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.get('/products', controller.products.getAllProducts);
+app.get('/products/:id', controller.products.getProductId);
+
+app.get('/sales', controller.sales.getAllSales);
+app.get('/sales/:id', controller.sales.getSaleId);
+
+app.use(throwMiddleware);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
