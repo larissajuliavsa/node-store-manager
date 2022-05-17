a023104f86f9c45779d543e9dfd5b45617357c7b
Larissa Araujo <larissa.jvsa@gmail.com>
2022-05-17 19:55:36 -0300

    feat: :ambulance: add delete sales/:id endpints, controller and joi validations

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const { throwMiddleware } = require('./middlewares/handleErrorMiddleware');
 const { validateProducts } = require('./middlewares/productsMiddleware');
+const { validateSales } = require('./middlewares/salesMiddlewares');
 const controller = require('./controllers');
 
 const app = express();
@@ -15,16 +16,15 @@ app.get('/', (_request, response) => {
 
 app.get('/products', controller.products.getAllProducts);
 app.get('/products/:id', controller.products.getProductId);
-
 app.post('/products', validateProducts, controller.products.createProduct);
-app.put('/products/:id', controller.products.updateProduct);
+app.put('/products/:id', validateProducts, controller.products.updateProduct);
 app.delete('/products/:id', controller.products.deleteProduct);
 
 app.get('/sales', controller.sales.getAllSales);
 app.get('/sales/:id', controller.sales.getSaleId);
-
-app.post('/sales', controller.sales.createSales);
-app.put('/sales/:id', controller.sales.updateSales);
+app.post('/sales', validateSales, controller.sales.createSales);
+app.put('/sales/:id', validateSales, controller.sales.updateSales);
+app.delete('/sales/:id', controller.sales.deleteSales);
 
 app.use(throwMiddleware);
 
