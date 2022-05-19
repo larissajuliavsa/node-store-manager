0281a638b7eba1cbd70989d54641de8d8513f3ed
Larissa Araujo <larissa.jvsa@gmail.com>
2022-05-19 14:23:54 -0300

    feat: :ambulance: add test/services sales returning object

--- test/unit/services/productsServices.js
+++ test/unit/services/productsServices.js
@@ -23,4 +23,39 @@ describe('Teste de Cobertura na Camada Product Services', () => {
     });
   });
 
+  describe('Service getAllProducts com produtos', () => {
+    const execute = [
+      {
+        "id": 1,
+        "name": "Martelo de Thor",
+        "quantity": 10
+      },
+    ];
+
+    before(() => {
+      sinon.stub(models, 'getAllProducts').resolves(execute)
+    });
+    
+    after(() => {
+      models.getAllProducts.restore();
+    });
+
+    it('getAllProducts retorna um array', async () => {
+      const response = await services.getAllProducts();
+
+      expect(response).to.be.an('array');
+    });
+
+    it('getAllProducts é um array de objeto', async () => {
+      const [response] = await services.getAllProducts();
+
+      expect(response).to.be.an('object');
+    });
+
+    it('getAllProducts retorna as chave id, name e quantity', async () => {
+      const [response] = await services.getAllProducts();
+
+      expect(response).to.have.all.keys('id', 'name', 'quantity')
+    });
+  });
 });
