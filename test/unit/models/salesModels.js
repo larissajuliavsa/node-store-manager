a10564fc6bc4cf16a935398ab038bff52000c8ca
Larissa Araujo <larissa.jvsa@gmail.com>
2022-05-19 14:20:22 -0300

    feat: :ambulance: add test/models sales returning object

--- test/unit/models/salesModels.js
+++ test/unit/models/salesModels.js
@@ -23,5 +23,41 @@ describe('Teste de Cobertura na Camada Sale Models', () => {
     });
   });
 
-
+  describe('Model getAllSales com vendas', () => {
+    const execute = [
+      {
+        "saleId": 1,
+        "date": "2021-09-09T04:54:29.000Z",
+        "productId": 1,
+        "quantity": 2
+      },
+      {
+        "saleId": 1,
+        "date": "2021-09-09T04:54:54.000Z",
+        "productId": 2,
+        "quantity": 2
+      }
+    ]
+  
+    before(() => {
+      sinon.stub(connection, 'execute').resolves(execute);
+    })
+  
+    after(() => {
+      connection.execute.restore();
+    });
+  
+    it('getAllSales retorna um array', async () => {
+      const response = await models.getAllSales();
+      expect(response).to.be.an('array');
+      expect(response).to.have.lengthOf(2);
+    });
+  
+    it('getAllSales retorna objeto com suas chaves', async () => {
+      const [response] = await models.getAllSales();
+      expect(response[0]).to.be.an('object');
+      expect(response).to.have.all.keys('saleId', 'date', 'productId', 'quantity')
+    });
+    
+  });
 });
