5906d6efce9148d27325d7b561f49918567278b0
Larissa Araujo <larissa.jvsa@gmail.com>
2022-05-19 14:17:12 -0300

    feat: :ambulance: add test/models productId

--- test/unit/models/productsModels.js
+++ test/unit/models/productsModels.js
@@ -24,4 +24,82 @@ describe('Teste de Cobertura na Camada Product Models', () => {
     });
   });
 
+  describe('Model getAllProducts com produtos', () => {
+    const execute = [
+      {
+        "id": 1,
+        "name": "Martelo de Thor",
+        "quantity": 10
+      },
+    ];
+  
+    before(() => {
+      sinon.stub(connection, 'execute').resolves(execute);
+    })
+  
+    after(() => {
+      connection.execute.restore();
+    });
+  
+    it('getAllProducts retorna um array', async () => {
+      const response = await models.getAllProducts();
+  
+      expect(response).to.be.an('array');
+    });
+  
+    it('getAllProducts é um array de objeto', async () => {
+      const [response] = await models.getAllProducts();
+  
+      expect(response).to.be.an('object');
+    });
+  
+    it('getAllProducts retorna as chave id, name e quantity', async () => {
+      const [response] = await models.getAllProducts();
+  
+      expect(response).to.have.all.keys('id', 'name', 'quantity')
+    });
+  });
+
+  describe('Model getProductId', () => {
+    const execute = [
+      {
+        "id": 1,
+        "name": "Martelo de Thor",
+        "quantity": 10
+      },
+    ];
+  
+    before(() => {
+      sinon.stub(connection, 'execute').resolves(execute);
+    })
+  
+    after(() => {
+      connection.execute.restore();
+    });
+  
+    it('getProductId retorna um array', async () => {
+      const response = await models.getProductId();
+  
+      expect(response).to.be.an('array');
+    });
+  
+    it('getProductId se não retorna um array vazio', async () => {
+      const response = await models.getProductId();
+  
+      expect(response).to.not.be.empty;
+    });
+  
+    it('getProductId é um array de objeto', async () => {
+      const [response] = await models.getProductId();
+  
+      expect(response).to.be.an('object');
+    });
+  
+    it('getProductId retorna as chave id, name e quantity', async () => {
+      const [response] = await models.getProductId();
+  
+      expect(response).to.have.all.keys('id', 'name', 'quantity')
+    });
+  
+  });
 });
